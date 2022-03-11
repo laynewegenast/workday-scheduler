c8844b781ed9c561b9dcf5472d5c211291420b00
macbeth <layne.wegenast@gmail.com>
2022-03-10 22:06:54 -0600

    past present

--- assets/script.js
+++ assets/script.js
@@ -8,9 +8,17 @@ $('.saveBtn').on('click', function(){
 
     localStorage.setItem(timeBlock, userInput)
 })
+var currentHour = moment().hours()
 
 for( let i = 9; i < 18; i++) {
     $('#' + i).val(localStorage.getItem(i))
+    if (i < currentHour) {
+        $('#' + i).addClass('past')
+    } else if (i == currentHour) {
+        $('#' + i).addClass('present')
+    } else {
+        $('#' + i).addClass('future')
+    }
 }
 
 //time blocks are color coded to indicate past present future
