7f41e01abbf89587ef50a6d31ec9270eda8cf2b0
macbeth <layne.wegenast@gmail.com>
2022-03-10 21:51:24 -0600

    save button text box

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 //display date at top of page
 $('#current-day').text(moment().format('dddd MMMM DD YYYY'));
 
+$('.saveBtn').on('click', function(){
+    var userInput = $(this).siblings('textarea').val()
+    var timeBlock = $(this).siblings('textarea').attr('id')
+    console.log(userInput, timeBlock);
+})
+
 //create time blocks for working hours of day
 
 //time blocks are color coded to indicate past present future
-var currentTime = Date.now().
-$('.time-row')
+
 //when time blocks are clicked the user may enter a task
 
 //tasks may be saved in local storage 
