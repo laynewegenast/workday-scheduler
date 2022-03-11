ff63765c57c216a93076f48b54ea84a785d1cf9c
macbeth <layne.wegenast@gmail.com>
2022-03-10 20:07:03 -0600

    timeblock add

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+//display date at top of page
+$('#current-day').text(moment().format('dddd MMMM DD YYYY'));
+
+//create time blocks for working hours of day
+
+//time blocks are color coded to indicate past present future
+var currentTime = Date.now().
+$('.time-row')
+//when time blocks are clicked the user may enter a task
+
+//tasks may be saved in local storage 
+
+//tasks remain on page when refreshed
